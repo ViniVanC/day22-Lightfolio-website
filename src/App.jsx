52ac8f220d3d45a3f9f291a29e1d6afbe47619a1
ViniVanC <vincentvan0302@gmail.com>
2023-04-06 17:29:42 +0300

    added TextBanner.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
+import { TextBanner } from "./components/TextBanner/TextBanner";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <TextBanner>
+        <span>A portfolio template for designers</span>
+        <span>·</span>
+        <a href="#">View all templates</a>
+      </TextBanner>
       <Header />
-    </div>
+    </>
   );
 }
 
