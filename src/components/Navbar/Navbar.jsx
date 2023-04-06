6dd53bff2911dbbf428b6784e5f0e8be255a1d22
ViniVanC <vincentvan0302@gmail.com>
2023-04-06 16:29:51 +0300

    created Navbar.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Navbar = ({
+  itemsList = [
+    {
+      text: "Work",
+      href: "#",
+    },
+    {
+      text: "About",
+      href: "#",
+    },
+    {
+      text: "Contact",
+      href: "#",
+    },
+  ],
+}) => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">
+        {itemsList?.map((item, i) => (
+          <li key={i} className="navbar__item">
+            <a className="navbar__link" href={item.href}>
+              {item.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
