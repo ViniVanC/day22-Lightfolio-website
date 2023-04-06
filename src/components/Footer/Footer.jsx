33576bd93517f40efbe45d1e27b41ea9d2c62c40
ViniVanC <vincentvan0302@gmail.com>
2023-04-06 21:02:35 +0300

    created Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Container } from "../Container";
+import { Navbar } from "../Navbar/Navbar";
+
+const socialNetworks = [
+  {
+    text: "Twitter",
+    href: "#",
+  },
+  {
+    text: "Instagram",
+    href: "#",
+  },
+  {
+    text: "LinkedIn",
+    href: "#",
+  },
+];
+
+const options = [
+  {
+    text: "Style guide",
+    href: "#",
+  },
+  {
+    text: "Licenses",
+    href: "#",
+  },
+  {
+    text: "Password",
+    href: "#",
+  },
+  {
+    text: "404",
+    href: "#",
+  },
+];
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <div className="footer__box footer__top">
+          <Navbar />
+          <Navbar itemsList={socialNetworks} />
+        </div>
+
+        <div className="footer__box footer__bottom">
+          <Navbar itemsList={options} />
+          <div className="footer__info">
+            <span>Built by Bryn Taylor</span>
+            <span>Powered by Webflow</span>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
+};
